a15156cd315cd219603efca6ab19f6a900137330
Peter Farley <far.peter1@gmail.com>
2014-01-11 17:38:22 -0600

    Added all instructions except ram/io instructions

--- src/cpu.d
+++ src/cpu.d
@@ -13,16 +13,26 @@ import std.stdio;
  *      - S - Sign
  *      - Z - Zero
  *      - P - Parity
- *      - C - Carry
+ *      - C - Carry (and/or link?)
  */
 
- enum
- {
- 	COND_INV  = 1, // Invert condition
- 	COND_ACCZ = 2, // Accumulator == 0
- 	COND_CALO = 4, // Carry or Link == 1
- 	COND_TEST = 8  // Test pin == 0
- }
+enum
+{
+	COND_INV  = 1, // Invert condition
+	COND_ACCZ = 2, // Accumulator == 0
+	COND_CALO = 4, // Carry or Link == 1
+	COND_TEST = 8  // Test pin == 0
+}
+
+enum
+{
+	STAT_SIGN   = 1, // Sign
+	STAT_ZERO   = 2, // Zero
+	STAT_PARITY = 4, // Parity
+	STAT_CARRY  = 8  // Carry
+}
+
+static ubyte[16] do_kbp = [ 0, 1, 2, 15, 3, 15, 15, 15, 4, 15, 15, 15, 15, 15, 15, 15 ];
 
 class cpu_4004
 {
@@ -39,15 +49,24 @@ class cpu_4004
 
 	ubyte status; // 4 bits
 
-	ubyte[4096] rom; // 4K of 8-bit words
+	ubyte[4096] rom; // 16 ROM chips, 256 bytes each
 
-	ubyte[640] ram; // 5120 bits of ram
+	ubyte[1280] ram; // 640 bytes
 
+	// Hardware:
 	ubyte test; // Test pin
+
+	// 4-bits
+	ubyte ram_addr = 0;
+	ubyte rom_addr = 0;
+
+	ubyte ram_cmdline = 0; // Which ram bank to use (3 bits)
+
+
 
 	this()
 	{
-		for(int i = 0; i < 16; i++) r[i] = 0;
+		r[0..15] = 0;
 		pc     = 0;
 		pc1    = 0;
 		pc2    = 0;
@@ -61,6 +80,14 @@ class cpu_4004
 		pc2 = pc1;
 		pc1 = pc;
 		pc  = new_pc;
+	}
+
+	private void sub_call_stack()
+	{
+		pc  = pc1;
+		pc1 = pc2;
+		pc2 = pc3;
+		pc3 = 0;
 	}
 
 	int next_instr()
@@ -83,18 +110,27 @@ class cpu_4004
 				if(jump)
 				{
 					// We are jumping within the same ROM
-					this.pc &= ~0xFFFF;
+					this.pc &= ~0xFF;
 					this.pc |= addr;
 				}
 				break;
 
-			case 0x20: // FIM -- fetch immediate
-				ubyte instr2 = rom[pc++]; // 2-byte instruction
-				ubyte reg = instr & 0x0E;
-				ubyte addr = instr2;
-				ubyte data = rom[(pc & ~0xFFFF) | addr];
-				r[reg]   = data >> 4 & 0x0F;
-				r[reg+1] = data & 0x0F;
+			case 0x20:
+				if(!(instr & 1)) // FIM -- fetch immediate
+				{
+					ubyte instr2 = rom[pc++]; // 2-byte instruction
+					ubyte reg = instr & 0x0E;
+					ubyte addr = instr2;
+					ubyte data = rom[(pc & ~0xFF) | addr];
+					r[reg]   = data >> 4 & 0x0F;
+					r[reg+1] = data & 0x0F;
+				}
+				else // SRC -- send register control
+				{
+					ubyte reg = instr & 0x0E;
+					ram_addr = cast(ubyte)(r[reg] << 4 | r[reg+1]);
+					rom_addr = cast(ubyte)(r[reg] << 4 | r[reg+1]);
+				}
 				break;
 
 			case 0x30:
@@ -109,7 +145,7 @@ class cpu_4004
 				else // JIN -- jump indirect
 				{
 					ubyte reg = instr & 0x0E;
-					pc &= ~0xFFFF;
+					pc &= ~0xFF;
 					pc |= cast(ubyte)(r[reg] << 4 | r[reg + 1]);
 				}
 				break;
@@ -124,6 +160,160 @@ class cpu_4004
 				ubyte instr2 = rom[pc++]; // 2-byte instruction
 				ushort addr = ((instr << 8) | instr2);
 				add_call_stack(addr);
+				break;
+
+			case 0x60: // INC -- increment register
+				ubyte reg = instr & 0x0F;
+				r[reg] = (r[reg] + 1) & 0x0F;
+				break;
+
+			case 0x70: // ISZ -- increment register, and jump if non-zero
+				ubyte instr2 = rom[pc++]; // 2-byte instruction
+				ubyte reg = instr & 0x0F;
+				r[reg] = (r[reg] + 1) & 0x0F;
+				if(r[reg])
+				{
+					ubyte addr = instr2;
+					pc &= ~0xFF;
+					pc |= addr;
+				}
+				break;
+
+			case 0x80: // ADD -- add register to accumulator
+				ubyte reg = instr & 0x0F;
+				a += r[reg] + ((status & STAT_CARRY) == STAT_CARRY);
+				if(a & 0xF0)
+				{
+					a &= 0x0F;
+					status |= STAT_CARRY;
+				}
+				else status &= ~STAT_CARRY;
+				break;
+
+			case 0x90: // SUB -- subtract register from accumulator
+				ubyte reg = instr & 0x0F;
+				ubyte oa = a;
+				a += (~r[reg]) & 0x0F + ((status & STAT_CARRY) == STAT_CARRY);
+				a &= 0x0F;
+				if(oa < (r[reg] & 0x0F)) status &= ~STAT_CARRY;
+				else status |= STAT_CARRY;
+				break;
+
+			case 0xA0: // LD -- load register into accumulator
+				ubyte reg = instr & 0x0F;
+				a = r[reg];
+				break;
+
+			case 0xB0: // XCH -- exchange register and accumulator
+				ubyte reg = instr & 0x0F;
+				ubyte temp = r[reg];
+				r[reg] = a;
+				a = temp;
+				break;
+
+			case 0xC0: // BBL -- branch back 1 level
+				ubyte data = instr & 0x0F;
+				sub_call_stack();
+				a = data;
+				break;
+
+			case 0xD0: // LDM -- load data into accumulator
+				ubyte data = instr & 0x0F;
+				a = data;
+				break;
+
+			case 0xE0: // Ram instructions
+				break;
+
+			case 0xF0:
+				switch(instr)
+				{
+					case 0xF0: // CLB: Clear accumulator and carry
+						a = 0;
+						status &= ~STAT_CARRY;
+						break;
+
+					case 0xF1: // CLC: Clear carry
+						status &= ~STAT_CARRY;
+						break;
+
+					case 0xF2: // IAC: Increment accumulator
+						a++;
+						if(a & 0xF0) status |= STAT_CARRY;
+						else status &= ~STAT_CARRY;
+						a &= 0x0F;
+						break;
+
+					case 0xF3: // CMC: Complement carry
+						if(status & STAT_CARRY) status &= ~STAT_CARRY;
+						else status |= STAT_CARRY;
+						break;
+
+					case 0xF4: // CMA: Complement accumulator
+						a = (~a) & 0x0F;
+						break;
+
+					case 0xF5: // RAL: Rotate accumulator and carry left
+						int c = status & STAT_CARRY;
+						int a3 = a & 0x08;
+						a <<= 1;
+						a &= 0x0F;
+						if(a3) status |= STAT_CARRY;
+						else status &= ~STAT_CARRY;
+						if(c)a |= 1;
+						else a &= ~1;
+						break;
+
+					case 0xF6: // RAR: Rotate accumulator and carry right
+						int c = status & STAT_CARRY;
+						int a0 = a & 0x01;
+						a >>= 1;
+						a &= 0x0F;
+						if(a0) status |= STAT_CARRY;
+						else status &= ~STAT_CARRY;
+						if(c)a |= 8;
+						else a &= ~8;
+						break;
+
+					case 0xF7: // TCC: Transmit carry to accumulator and clear carry
+						a = status & STAT_CARRY;
+						status &= ~STAT_CARRY;
+						break;
+
+					case 0xF8: // DAC: Decrement accumulator
+						if(a == 0) status &= ~STAT_CARRY;
+						else status |= STAT_CARRY;
+						a = (a - 1) & 0x0F;
+						break;
+
+					case 0xF9: // TCS: Transfer carry subtract and clear carry
+						if(status & STAT_CARRY) a = 10;
+						else a = 9;
+						status &= ~STAT_CARRY;
+						break;
+
+					case 0xFA: // STC: Set carry
+						status |= STAT_CARRY;
+						break;
+
+					case 0xFB: // DAA: Decimal adjust accumulator
+						if(status & STAT_CARRY || a > 9) a += 6;
+						if(a & 0xF0) status |= STAT_CARRY;
+						a &= 0x0F;
+						break;
+
+					case 0xFC: // KBP: Keyboard process
+						a = do_kbp[a];
+						break;
+
+					case 0xFD: // DCL: Designate command line
+						ram_cmdline = a & 0x07;
+						break;
+
+					default:
+						// Error handle here?
+						break;
+				}
 				break;
 
 			default:
